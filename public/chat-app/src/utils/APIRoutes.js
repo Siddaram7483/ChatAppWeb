6f3462265d1b9c606d8d363f6cae784285156b4b
Siddaram Mahadev Kusur <117756982+Siddaram7483@users.noreply.github.com>
2024-11-07 16:38:09 +0530

    Update APIRoutes.js

--- public/chat-app/src/utils/APIRoutes.js
+++ public/chat-app/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
- export const host = "http://localhost:5000";
+ export const host = "https://chatappweb-vbyj.onrender.com";
 export const registerRoute =`${host}/api/auth/register`;
 export const LoginRoute =`${host}/api/auth/login`;
 export const setAvatarRoutes =`${host}/api/auth/setAvatar`;
